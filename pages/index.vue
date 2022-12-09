dee8e7e431e988bb76ae9f00c968f5071e9cf8cd
Utiyama Leandro <lutiyama@gmail.com>
2022-12-10 08:55:50 +0900

    Initial

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <Video :videoList="videos"/>
+  </div>
+  </template>
+  <script>
+  import Video from "@/components/Video.vue"
+  export default {
+    components: {
+      Video
+    },
+  head: {
+      title: "Home"
+    },
+      data() {
+        return {
+          videos: []
+        }
+      },
+      async fetch() {
+        this.videos = await fetch(
+          '/video'
+        ).then(res => res.json())
+      }
+  }
+  </script>
